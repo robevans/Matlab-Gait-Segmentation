9a6c6292a95f01e0a7dd6a48cfc5c224c87fe99c
Robert Evans <rlevans91@gmail.com>
2014-02-05 02:23:51 +0000

    Added option to plot ground truth beneath segmentation results

---
+++ NN_HMM_Segmenter/plotClassesAndGroundTruth.m
@@ -0,0 +1,78 @@
+function plotClassesAndGroundTruth(data, classes, trueSegments, window_size, plotTitle)
+
+if nargin < 5
+    plotTitle = 'Motion data';
+end
+
+% Create figure
+screen_size = get(0,'ScreenSize');
+figure1 = figure('Name',...
+    'Segmentation with Neural Network and Hidden Markov Model', 'OuterPosition',[1 screen_size(4)/2 screen_size(3)/2 screen_size(4)/2]);
+
+%% Create subplot for sensor data
+subplot1 = subplot(3,1,1,'Parent',figure1,'XMinorTick','on');
+
+box(subplot1,'on');
+grid(subplot1,'on');
+hold(subplot1,'all');
+
+% Uncomment the following lines to preserve the limits of the axes
+%xlim(subplot1,[32 370]);
+%ylim(subplot1,[-6.89929328621908 16.7932862190813]);
+
+% Create multiple lines using matrix input to plot
+plot1 = plot(data,'Parent',subplot1);
+
+% Create ylabel
+ylabel('Rotation (deg/s)');
+
+% Create title
+title(plotTitle);
+
+%% Create subplot for segment boundaries
+subplot2 = subplot(3,1,2,'Parent',figure1,...
+    ...%'YTickLabel',{'','Heel-off to Toe-off','Toe-off to Midswing','Midswing to Heel-strike','Heel-strike to Toe-down','Toe-down to Midstance','Midstance to Heel-off'},...
+    'XMinorTick','on');
+
+box(subplot2,'on');
+grid(subplot2,'on');
+hold(subplot2,'all');
+
+% Create stem plot
+stem(classes,'Parent',subplot2);
+
+% Create title
+title('Segmentation');
+
+% Create legend
+%legend(subplot2,'show');
+
+%% Create subplot for ground truth
+subplot3 = subplot(3,1,3,'Parent',figure1,...
+    ...%'YTickLabel',{'','Heel-off to Toe-off','Toe-off to Midswing','Midswing to Heel-strike','Heel-strike to Toe-down','Toe-down to Midstance','Midstance to Heel-off'},...
+    'XMinorTick','on');
+
+% Format data for plotting
+[~, T] = formatForNetwork(data, trueSegments, 1, 1);
+trueClasses = realignNetworkOutput(T, 1, 1, length(data));
+trueClasses(1:floor(window_size/2)) = 0;
+trueClasses(end-floor(window_size/2)+1:end) = 0;
+
+box(subplot3,'on');
+grid(subplot3,'on');
+hold(subplot3,'all');
+
+% Create stem plot
+stem(trueClasses,'Parent',subplot3);
+
+% Create xlabel
+xlabel('Time (frames)');
+
+% Create title
+title('Ground Truth');
+
+% Create legend
+%legend(subplot3,'show');
+
+% Link x-axes of subplots.
+linkaxes([subplot1,subplot2,subplot3], 'x');
