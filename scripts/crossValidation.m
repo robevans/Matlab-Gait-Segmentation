d8e760e5c21ec173cb1a4b2b5aaa3cd111997b39
Robert Evans <rlevans91@gmail.com>
2014-02-07 02:08:01 +0000

    Option to save plots but not display them works better now - it only shows one figure rather than producing all 40

--- scripts/crossValidation.m
+++ scripts/crossValidation.m
@@ -1,5 +1,14 @@
-function average_performance = doCrossValidation(showPlots, savePlots, window_size, hiddenLayers)
+function average_performance = doCrossValidation(showPlots, savePlots, window_size, hiddenLayers, data_columns)
 %Does cross validation for Vicon aligned Orient data.
+
+if nargin < 5
+    data_columns = 1:42;
+end
+
+if savePlots && ~showPlots
+    showPlots = true;
+    closePlotAfterSaving = true;
+end
 
 % Model parameters
 %hiddenLayers = [10,10,10];
@@ -12,6 +21,10 @@ titles = {'M10' 'M18' 'M19' 'M23' 'T6' 'T9' 'T12' 'T13' 'R11' 'R14' 'R15' 'R20' 
 data = {M10 M18 M19 M23 T6 T9 T12 T13 R11 R14 R15 R20 S5 S7 S10 S14 J4 J6 J14 J15};
 Lsegs = {Msegs10L Msegs18L Msegs19L Msegs23L Tsegs6L Tsegs9L Tsegs12L Tsegs13L Rsegs11L Rsegs14L Rsegs15L Rsegs20L Ssegs5L Ssegs7L Ssegs10L Ssegs14L Jsegs4L Jsegs6L Jsegs14L Jsegs15L};
 Rsegs = {Msegs10R Msegs18R Msegs19R Msegs23R Tsegs6R Tsegs9R Tsegs12R Tsegs13R Rsegs11R Rsegs14R Rsegs15R Rsegs20R Ssegs5R Ssegs7R Ssegs10R Ssegs14R Jsegs4R Jsegs6R Jsegs14R Jsegs15R};
+
+for i = 1 : length(titles)
+    data{i} = data{i}(:,data_columns);
+end
 
 sum_performance = 0;
 
@@ -31,10 +44,16 @@ for i = 1 : length(data)
     [~, perfL] = buildTrainTestNNAndHMM_cellArrayInputs(train_data, train_Lsegs, train_data, train_Lsegs, test_data, test_Lsegs, hiddenLayers, step_size, window_size, 'trainscg', strcat('Left Leg Segments - capture ',{' '},titles{i}), showPlots);
     if savePlots
         print( strcat('./Graphs/CrossVal/',titles{i},'L'), '-dpdf')
+        if closePlotAfterSaving
+            close
+        end
     end
     [~, perfR] = buildTrainTestNNAndHMM_cellArrayInputs(train_data, train_Rsegs, train_data, train_Rsegs, test_data, test_Rsegs, hiddenLayers, step_size, window_size, 'trainscg', strcat('Right Leg Segments - capture ',{' '},titles{i}), showPlots);
     if savePlots
         print( strcat('./Graphs/CrossVal/',titles{i},'R'), '-dpdf')
+        if closePlotAfterSaving
+            close
+        end
     end
     
     sum_performance = sum_performance + perfL + perfR;
