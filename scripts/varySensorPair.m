2578af974223f8d460d6a1681c954ca84f190dad
Robert Evans <rlevans91@gmail.com>
2014-02-11 19:48:22 +0000

    Script now calculates specificity and sensitivity performance for each sensor combination, and for each gait phase. The next step is to output nice graphs

--- scripts/varySensorPair.m
+++ scripts/varySensorPair.m
@@ -1,14 +1,46 @@
+function varySensorPair(hiddenLayers, window_size, performanceCountsTolerance)
 % Script to test performance effects of varying sensor placement.
-loadAlignedData
+% Plots sensitivity and specificity for each sensor placement, for each
+% phase of the gait.
 
-NeuralNetworkHiddenLayers = [10,10,10];
-window_size = 15;
+if nargin < 1
+    hiddenLayers = [10,10,10];
+end
+if nargin < 2
+    window_size = 15;
+end
+if nargin <3
+    performanceCountsTolerance = 0;
+end
 
-perf_all_sensors = crossValidation(0,1, window_size, NeuralNetworkHiddenLayers);
-perf_thigh = crossValidation(0,1, window_size, NeuralNetworkHiddenLayers, upLegCols);
-perf_shin = crossValidation(0,1, window_size, NeuralNetworkHiddenLayers, legCols);
-perf_foot = crossValidation(0,1, window_size, NeuralNetworkHiddenLayers, feetCols);
+loadAlignedData('caller')
 
-save perfSensorPlacement perf_all_sensors perf_thigh perf_shin perf_foot
+perfCounts_all_sensors = crossValidation(0,0, window_size, hiddenLayers, performanceCountsTolerance);
+perfCounts_pelvis = crossValidation(0,0, window_size, hiddenLayers, performanceCountsTolerance, pelvisCols);
+perfCounts_thigh = crossValidation(0,0, window_size, hiddenLayers, performanceCountsTolerance, upLegCols);
+perfCounts_shin = crossValidation(0,0, window_size, hiddenLayers, performanceCountsTolerance, legCols);
+perfCounts_foot = crossValidation(0,0, window_size, hiddenLayers, performanceCountsTolerance, feetCols);
 
-close all
+[all_sensitivity, all_specificity] = getSensitivityAndSpecificityForEachClass(perfCounts_all_sensors);
+[pelvis_sensitivity, pelvis_specificity] = getSensitivityAndSpecificityForEachClass(perfCounts_pelvis);
+[thigh_sensitivity, thigh_specificity] = getSensitivityAndSpecificityForEachClass(perfCounts_thigh);
+[shin_sensitivity, shin_specificity] = getSensitivityAndSpecificityForEachClass(perfCounts_shin);
+[foot_sensitivity, foot_specificity] = getSensitivityAndSpecificityForEachClass(perfCounts_thigh);
+
+figure
+plot([all_sensitivity, pelvis_sensitivity])%, thigh_sensitivity, shin_sensitivity, foot_sensitivity])
+figure;
+plot([all_sensitivity, pelvis_sensitivity, thigh_sensitivity, shin_sensitivity, foot_sensitivity])
+
+save perfSensorPlacement perfCounts_all_sensors perfCounts_thigh perfCounts_shin perfCounts_foot
+
+end
+
+function [classes_sensitivity, classes_specificity] = getSensitivityAndSpecificityForEachClass(sumOfPerformanceCountsByClass)
+nClasses = size(sumOfPerformanceCountsByClass,1);
+classes_sensitivity = zeros(nClasses, 1);
+classes_specificity = zeros(nClasses, 1);
+for class = 1 : nClasses
+    [classes_sensitivity(class), classes_specificity(class)] = getSensitivityAndSpecificity( sumOfPerformanceCountsByClass(class,:) );
+end
+end
