529a1dad0e7436b69f49862f494619e75a7dd6e0
Robert Evans <rlevans91@gmail.com>
2014-02-15 01:33:36 +0000

    Working version to make graphs for a given tolerance

--- scripts/varySensorPair.m
+++ scripts/varySensorPair.m
@@ -15,7 +15,7 @@ end
 
 loadAlignedData('caller')
 
-% Generate data
+% Generate performance data
 [allSn,allSp] = crossValidateAndEvaluate(window_size, hiddenLayers, performanceCountsTolerance, allCols);
 [pelSn,pelSp] = crossValidateAndEvaluate(window_size, hiddenLayers, performanceCountsTolerance, pelvisCols);
 [thiSn,thiSp] = crossValidateAndEvaluate(window_size, hiddenLayers, performanceCountsTolerance, upLegCols);
@@ -35,22 +35,24 @@ loadAlignedData('caller')
 [fooGySn,fooGySp] = crossValidateAndEvaluate(window_size, hiddenLayers, performanceCountsTolerance, feetGyroCols);
 
 % Create figures
-createAndSaveFigure([allSn,pelSn,thiSn,shiSn,fooSn],'Sensor placement Sensitivity with Accel + Gyro data','Sensitivity (%)',{'All','Pelvis','Thighs','Legs','Feet'})
-createAndSaveFigure([allSp,pelSp,thiSp,shiSp,fooSp],'Sensor placement Specificity with Accel + Gyro data','Specificity (%)',{'All','Pelvis','Thighs','Legs','Feet'})
+createAndSaveFigure([allSn,pelSn,thiSn,shiSn,fooSn],strcat('Sensor placement Sensitivity with Accel + Gyro data (Tolerance',{' '},num2str(performanceCountsTolerance),')'),'Sensitivity (%)',{'All','Pelvis','Thighs','Legs','Feet'},['SensitivityAccelGyroTolerance' num2str(performanceCountsTolerance)])
+createAndSaveFigure([allSp,pelSp,thiSp,shiSp,fooSp],strcat('Sensor placement Specificity with Accel + Gyro data, (Tolerance',{' '},num2str(performanceCountsTolerance),')'),'Specificity (%)',{'All','Pelvis','Thighs','Legs','Feet'},['SpecificityAccelGyroTolerance' num2str(performanceCountsTolerance)])
 
-createAndSaveFigure([allAcSn,pelAcSn,thiAcSn,shiAcSn,fooAcSn],'Sensor placement Sensitivity with Accel data','Sensitivity (%)',{'All','Pelvis','Thighs','Legs','Feet'})
-createAndSaveFigure([allAcSp,pelAcSp,thiAcSp,shiAcSp,fooAcSp],'Sensor placement Specificity with Accel data','Specificity (%)',{'All','Pelvis','Thighs','Legs','Feet'})
+createAndSaveFigure([allAcSn,pelAcSn,thiAcSn,shiAcSn,fooAcSn],strcat('Sensor placement Sensitivity with Accel data, (Tolerance',{' '},num2str(performanceCountsTolerance),')'),'Sensitivity (%)',{'All','Pelvis','Thighs','Legs','Feet'},['SensitivityAccelTolerance' num2str(performanceCountsTolerance)])
+createAndSaveFigure([allAcSp,pelAcSp,thiAcSp,shiAcSp,fooAcSp],strcat('Sensor placement Specificity with Accel data, (Tolerance',{' '},num2str(performanceCountsTolerance),')'),'Specificity (%)',{'All','Pelvis','Thighs','Legs','Feet'},['SpecificityAccelTolerance' num2str(performanceCountsTolerance)])
 
-createAndSaveFigure([allGySn,pelGySn,thiGySn,shiGySn,fooGySn],'Sensor placement Sensitivity with Gyro data','Sensitivity (%)',{'All','Pelvis','Thighs','Legs','Feet'})
-createAndSaveFigure([allGySp,pelGySp,thiGySp,shiGySp,fooGySp],'Sensor placement Specificity with Gyro data','Specificity (%)',{'All','Pelvis','Thighs','Legs','Feet'})
+createAndSaveFigure([allGySn,pelGySn,thiGySn,shiGySn,fooGySn],strcat('Sensor placement Sensitivity with Gyro data, (Tolerance',{' '},num2str(performanceCountsTolerance),')'),'Sensitivity (%)',{'All','Pelvis','Thighs','Legs','Feet'},['SensitivityGyroTolerance' num2str(performanceCountsTolerance)])
+createAndSaveFigure([allGySp,pelGySp,thiGySp,shiGySp,fooGySp],strcat('Sensor placement Specificity with Gyro data, (Tolerance',{' '},num2str(performanceCountsTolerance),')'),'Specificity (%)',{'All','Pelvis','Thighs','Legs','Feet'},['SpecificityGyroTolerance' num2str(performanceCountsTolerance)])
+
+save( ['placementsTolerance' num2str(performanceCountsTolerance)], 'allSn', 'allSp', 'pelSn', 'pelSp', 'thiSn', 'thiSp', 'shiSn', 'shiSp', 'fooSn', 'fooSp', 'allAcSn', 'allAcSp', 'pelAcSn', 'pelAcSp', 'thiAcSn', 'thiAcSp', 'shiAcSn', 'shiAcSp', 'fooAcSn', 'fooAcSp', 'allGySn', 'allGySp', 'pelGySn', 'pelGySp', 'thiGySn', 'thiGySp', 'shiGySn', 'shiGySp', 'fooGySn', 'fooGySp')
 
 end
 
 function crossValidateEvaluateAndPlotOneData(window_size, hiddenLayers, performanceCountsTolerance, data_columns, plotTitle)
 perfCounts = crossValidation(0,0, window_size, hiddenLayers, performanceCountsTolerance, data_columns);
 [sensitivity, specificity] = getSensitivityAndSpecificityForEachClass(perfCounts);
-createAndSaveFigure(sensitivity, plotTitle, 'Sensitivity (%)',0)
-createAndSaveFigure(specificity, plotTitle, 'Specificity (%)',0)
+createAndSaveFigure(sensitivity, plotTitle, 'Sensitivity (%)',0,plotTitle)
+createAndSaveFigure(specificity, plotTitle, 'Specificity (%)',0,plotTitle)
 end
 
 function [sensitivity, specificity] = crossValidateAndEvaluate(window_size, hiddenLayers, performanceCountsTolerance, data_columns)
@@ -67,12 +69,12 @@ for class = 1 : nClasses
 end
 end
 
-function createAndSaveFigure(YMatrix1, titleString, yLabelString, legend)
+function createAndSaveFigure(YMatrix1, titleString, yLabelString, legend, savedFigFilename)
 createfigure(YMatrix1, titleString, yLabelString, legend)
 set(gcf,'PaperPositionMode','auto');
 set(gcf,'PaperOrientation','landscape');
 set(gcf,'Position',[50 50 1200 800]);
-print( strcat('./Graphs/Sensor Pair/',titleString), '-dpdf')
+print( strcat('./Graphs/Sensor Pair/',savedFigFilename), '-dpdf')
 end
 
 function createfigure(YMatrix1, titleString, yLabelString, legend_labels)
@@ -93,7 +95,7 @@ axes1 = axes('Parent',figure1,...
     'FontSize',12);
 %% Uncomment the following line to preserve the X-limits of the axes
 xlim(axes1,[0.8 5.2]);
-ylim(axes1,[90 100]);
+ylim(axes1,[0 100]);
 box(axes1,'on');
 hold(axes1,'all');
 
