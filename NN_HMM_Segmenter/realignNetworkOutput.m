6e8211d8fd3cfa87decb454965566350e27282b0
Robert Evans <rlevans91@gmail.com>
2014-03-10 20:19:21 +0000

    Vectorised slow realignment code

--- NN_HMM_Segmenter/realignNetworkOutput.m
+++ NN_HMM_Segmenter/realignNetworkOutput.m
@@ -5,12 +5,13 @@ function [ classes ] = realignNetworkOutput( windows, step_size, window_size, le
 % Assumes that sliding windows were classified based on their central
 % index.
 
+windows = vec2ind(windows);
 classes = zeros(1, length_of_original_input);
+
 
 for i = 1 : length(windows)
     original_index = (i-1)*step_size+floor(window_size/2)+1;
-    class = vec2ind(windows(:,i));
-    classes(original_index) = class;
+    classes(original_index) = windows(i);
 end
 
 end
